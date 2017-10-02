8b3ad73fee5a456c512f93104d0b1db4d386914d
Noxoruz <mccully.matthew@gmail.com>
2017-10-02 11:31:02 +0200

    hi

--- DA_Lab_2.cpp
+++ DA_Lab_2.cpp
@@ -7,6 +7,9 @@
 using namespace std;
 
 
+//0x33 & 0xF0 = (0000 0000 0011 0011 & 0000 0000 1111 0011 = 0000 0000 0011 0000) = 0x30
+//0x33 | 0xF0 = (0000 0000 0011 0011 | 0000 0000 1111 0000 = 0000 0000 1111 0011) = 0xF3
+//0x33 ^ 0xF0 = (0000 0000 0011 0011 ^ 0000 0000 1111 0000 = 0000 0000 1100 0011) = 0xC3
 
 template <typename T> void printlnbits(T v)
 {
@@ -34,17 +37,24 @@ int main()
 	const float d = -0.0f;
 	const long long unsigned e = 1LLU << 40;
 	const foo f;
+	cout << "Printing char a: \t\t\t\t\t\t";
 	printlnbits(a);
 	cout << a << endl;
+	cout << "Printing int b -1L: \t\t\t\t\t\t";
 	printlnbits(b);
 	cout << b << endl;
+	cout << "Printing const unsigned c 1 << 31: \t\t\t\t";
 	printlnbits(c);
 	cout << b << endl;
+	cout << "Printing float d -0.0f: \t\t\t\t\t";
 	printlnbits(d);
 	cout << d << endl;
+	cout << "Printing const long long unsigned e = 1LLU << 40: \t\t";
 	printlnbits(e);
 	cout << e << endl;
+	cout << "Printing const foo f: \t\t\t\t\t\t";
 	printlnbits(f);
+	cout << "F.a: "  <<f.a << "\tF.b: " << f.b << "\tF.x: " << f.x << endl;
 	char ok;
 	cin >> ok;
     return 0;
